d1f170c5895decb03d5290f039c7d136c9d75433
Ahmed ZeinEddine <ahmad.zeineddine@hotmail.com>
2017-05-24 00:27:07 +0300

    Initial commit

---
+++ app/src/main/java/com/ahmad/kotlinapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.ahmad.kotlinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val people = listOf(Person("hiba", 23), Person("Hassan", 25))
+        trackBtn.setOnClickListener {
+            println("Hello world, I am Kotlin")
+            val eldest = people.maxBy { it.age }
+            if(eldest!=null) println("The eldest is "+eldest.name)
+        }
+    }
+    data class Person(val name:String, val age:Int=0)
+}
